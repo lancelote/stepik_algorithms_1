5e4f53ab723582943696b5395875d17838878688
Pavel Karateev <lancelote.du.lac@gmail.com>
2019-09-15 18:13:39 +0300

    WIP: Python Huffman
    + Remove needless code
    + Complete tree generation code

--- python/src/module_4/huffman_code.py
+++ python/src/module_4/huffman_code.py
@@ -3,61 +3,49 @@ from __future__ import annotations
 import heapq
 from collections import Counter
 from dataclasses import dataclass
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
 
 @dataclass
 class Node:
-    value: int
+    value: int = 0
+    code: str = ''
+
+    def __gt__(self, other: None):
+        assert isinstance(other, Node)
+        return self.value > other.value
 
 
 @dataclass
 class Branch(Node):
-    lf: Node
-    ri: Node
+    lf: Optional[Node] = None
+    ri: Optional[Node] = None
 
 
 @dataclass
 class Leaf(Node):
-    char: str
-    value: int
-    code: str = ''
+    char: Optional[str] = None
 
 
 def huffman(frequencies: List[Tuple[str, int]]):
-    heap = [Leaf(char, value) for (char, value) in frequencies]
+    heap = [Leaf(value=value, char=char) for (char, value) in frequencies]
     heapq.heapify(heap)
 
     while len(heap) > 1:
         lf: Node = heapq.heappop(heap)
         ri: Node = heapq.heappop(heap)
-        branch = Branch(lf.value + ri.value, lf, ri)
+        lf.code += '0'
+        ri.code += '1'
+        branch = Branch(value=lf.value + ri.value, lf=lf, ri=ri)
         heapq.heappush(heap, branch)
-
-
-def encode(frequency):
-    print(frequency)
-    heap = [[weight, [symbol, '']] for symbol, weight in frequency]
-    heapq.heapify(heap)
-    while len(heap) > 1:
-        print(heap)
-        low = heapq.heappop(heap)
-        high = heapq.heappop(heap)
-        for value in low[1:]:
-            value[1] = '0' + value[1]
-        for value in high[1:]:
-            value[1] = '1' + value[1]
-        heapq.heappush(heap, [low[0] + high[0]] + low[1:] + high[1:])
-    print(heap)
-    return sorted(heapq.heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
+    return heap
 
 
 def main():
     frequency = Counter("abacabad").most_common()
-    huff = encode(frequency)
-    print(huff)
+    tree = huffman(frequency)
+    print(tree)
 
 
 if __name__ == '__main__':
-    # ToDo: try heapq + recursive approach
     main()
