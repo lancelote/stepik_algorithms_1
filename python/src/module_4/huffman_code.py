cfff5574377e90038b92cdab5a8d4f2720ef9e17
Pavel Karateev <lancelote.du.lac@gmail.com>
2019-09-26 11:37:26 +0300

    #4 Python Huffman
    + Create a Tree class to store the relevant logic

--- python/src/module_4/huffman_code.py
+++ python/src/module_4/huffman_code.py
@@ -27,24 +27,36 @@ class Leaf(Node):
     char: Optional[str] = None
 
 
-def huffman(frequencies: List[Tuple[str, int]]):
-    heap = [Leaf(value=value, char=char) for (char, value) in frequencies]
-    heapq.heapify(heap)
+@dataclass
+class Tree:
+    root: Node
 
-    while len(heap) > 1:
-        lf: Node = heapq.heappop(heap)
-        ri: Node = heapq.heappop(heap)
-        lf.code += '0'
-        ri.code += '1'
-        branch = Branch(value=lf.value + ri.value, lf=lf, ri=ri)
-        heapq.heappush(heap, branch)
-    return heap
+    @classmethod
+    def from_string(cls, string) -> Tree:
+        frequency = Counter(string).most_common()
+        return Tree(cls.construct_tree(frequency))
+
+    @classmethod
+    def construct_tree(cls, frequencies: List[Tuple[str, int]]) -> Node:
+        """Construct a huffman binary tree."""
+
+        heap = [Leaf(value=value, char=char) for (char, value) in frequencies]
+        heapq.heapify(heap)
+
+        while len(heap) > 1:
+            lf: Node = heapq.heappop(heap)
+            ri: Node = heapq.heappop(heap)
+            lf.code += '0'
+            ri.code += '1'
+            branch = Branch(value=lf.value + ri.value, lf=lf, ri=ri)
+            heapq.heappush(heap, branch)
+        assert len(heap) == 1
+        return heap[0]
 
 
 def main():
-    frequency = Counter("abacabad").most_common()
-    tree = huffman(frequency)
-    print(tree)
+    tree = Tree.from_string("abacabad")
+    print(tree.root)
 
 
 if __name__ == '__main__':
