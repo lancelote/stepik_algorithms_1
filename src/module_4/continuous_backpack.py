2f6698e22d2aa4ea03f9c90e65ca7f573d715f0a
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-06-26 16:14:04 +0300

    Update backpack solution

--- src/module_4/continuous_backpack.py
+++ src/module_4/continuous_backpack.py
@@ -5,27 +5,31 @@ class Thing(NamedTuple):
     price: int
     weight: int
 
+    @property
+    def unit_cost(self) -> float:
+        return self.price / self.weight
+
 
 def max_value(capacity: int, data: List[Thing]) -> float:
     total_value = 0.0
-    things = sorted(data, key=lambda x: x.price / x.weight, reverse=True)
+    things = sorted(data, key=lambda x: x.unit_cost, reverse=True)
 
     for thing in things:
-        if thing.weight > capacity:
-            total_value += capacity * (thing.price / thing.weight)
+        weight = min(thing.weight, capacity)
+        total_value += weight * thing.unit_cost
+        capacity -= weight
+
+        if capacity == 0:
             break
-        else:
-            total_value += thing.price
-            capacity -= thing.weight
     return round(total_value, 3)
 
 
-def main():
+def main() -> None:
     data = list()
-    n, capacity = map(int, input().split())
+    n, capacity = [int(x) for x in input().split()]
 
     for _ in range(n):
-        price, weight = map(int, input().split())
+        price, weight = [int(x) for x in input().split()]
         data.append(Thing(price, weight))
 
     print(max_value(capacity, data))
