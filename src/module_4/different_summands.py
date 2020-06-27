231052f5bbf8240c4afd09e18687996e17f7e430
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-06-27 14:06:27 +0300

    Update different summands solution

--- src/module_4/different_summands.py
+++ src/module_4/different_summands.py
@@ -7,27 +7,26 @@ def different_summands(number: int) -> List[int]:
     remaining = number
     summands = []
 
-    while True:
-        if remaining == 0:
-            break
+    while remaining:
         # If after a summand subtraction we will end up with a number which is
         # less or equal to summand itself we will be stuck as the remaining
         # part can not be expressed with new unique summands so we have to stop
-        # here and simply and remaining part as one big summand
+        # here and simply add remaining part as one big summand
         if remaining - summand <= summand:
             summands.append(remaining)
             break
+
         summands.append(summand)
         remaining -= summand
         summand += 1
     return summands
 
 
-def main():
+def main() -> None:
     number = int(input())
     summands = different_summands(number)
     print(len(summands))
-    print(" ".join(map(str, summands)))
+    print(" ".join(str(x) for x in summands))
 
 
 if __name__ == "__main__":
--- tests/module_4/test_different_summands.py
+++ tests/module_4/test_different_summands.py
@@ -3,6 +3,6 @@ import pytest
 from src.module_4.different_summands import different_summands
 
 
-@pytest.mark.parametrize("n,summands", [(4, [1, 3]), (6, [1, 2, 3]),])
+@pytest.mark.parametrize("n,summands", [(4, [1, 3]), (6, [1, 2, 3])])
 def test_different_summands(n, summands):
     assert different_summands(n) == summands
