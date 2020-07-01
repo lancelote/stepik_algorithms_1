4dd6b4af17b8008a6e8f73313c18e1a5c2509137
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-07-01 23:08:57 +0300

    Missing annotations

--- src/module_4/huffman_code.py
+++ src/module_4/huffman_code.py
@@ -1,64 +1,66 @@
+from __future__ import annotations
+
 import heapq
 from abc import abstractmethod, ABCMeta
 from collections import Counter
-from typing import List, Tuple, Optional, Dict, Generator
+from typing import List, Tuple, Optional, Dict, Iterator
 
 
 class Node(metaclass=ABCMeta):
-    def __init__(self, value: int = 0, code: str = ""):
+    def __init__(self, value: int = 0, code: str = "") -> None:
         self.value = value
         self.code = code
 
-    def __gt__(self, other: None):
+    def __gt__(self, other: None) -> bool:
         assert isinstance(other, Node)
         return self.value > other.value
 
     @abstractmethod
-    def walk(self, code: str = "") -> Generator[Tuple[str, str], None, None]:
+    def walk(self, code: str = "") -> Iterator[Tuple[str, str]]:
         """Walk along the tree and collect codes for each character."""
         ...
 
 
 class Branch(Node):
-    def __init__(self, lf: Node, ri: Node, **kwargs):
+    def __init__(self, lf: Node, ri: Node, **kwargs) -> None:
         super().__init__(**kwargs)
         self.lf = lf
         self.ri = ri
 
-    def walk(self, code: str = "") -> Generator[Tuple[str, str], None, None]:
+    def walk(self, code: str = "") -> Iterator[Tuple[str, str]]:
         yield from self.lf.walk(code + self.code)
         yield from self.ri.walk(code + self.code)
 
 
 class Leaf(Node):
-    def __init__(self, char: str, **kwargs):
+    def __init__(self, char: str, **kwargs) -> None:
         super().__init__(**kwargs)
         self.char = char
 
-    def walk(self, code: str = "") -> Generator[Tuple[str, str], None, None]:
-        yield (self.char, code + self.code)
+    def walk(self, code: str = "") -> Iterator[Tuple[str, str]]:
+        yield self.char, code + self.code
 
 
 class Tree:
-    def __init__(self, root: Node, string: str):
+    def __init__(self, root: Node, string: str) -> None:
         self.root = root
         self.string = string
         self._encode_dict = self._construct_encode_dict()
 
     @classmethod
-    def from_string(cls, string) -> "Tree":
+    def from_string(cls, string: str) -> Tree:
         frequency = Counter(string).most_common()
         tree = Tree(root=cls._construct_tree(frequency), string=string)
         tree._construct_encode_dict()
         return tree
 
-    def get_encoded_string(self):
+    def get_encoded_string(self) -> str:
         return "".join(self._encode_dict[char] for char in self.string)
 
-    def get_encode_dict_length(self):
+    def get_encode_dict_length(self) -> int:
         return len(self._encode_dict)
 
-    def print_encode_dict(self):
+    def print_encode_dict(self) -> None:
         for k, v in self._encode_dict.items():
             print(f"{k}: {v}")
 
@@ -87,7 +89,7 @@ class Tree:
         return {char: code for (char, code) in self.root.walk()}
 
 
-def main():
+def main() -> None:
     string = input().strip()
     tree = Tree.from_string(string)
     encoded_string = tree.get_encoded_string()
