d0300164a4a4a1936865ab767c02764b8db42fe2
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-07-05 20:24:48 +0300

    Add tests for huffman encoding

---
+++ tests/module_4/test_huffman_encode.py
@@ -0,0 +1,11 @@
+import pytest
+
+from src.module_4.huffman_code import Tree
+
+
+@pytest.mark.parametrize("string,expected", [
+    ("a", "0"),
+    ("abacabad", "01001100100111"),
+])
+def test_encoding(string, expected):
+    assert Tree.from_string(string).get_encoded_string() == expected
