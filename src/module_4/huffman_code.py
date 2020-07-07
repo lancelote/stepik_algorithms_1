f367e6cb5bbe5701617e9146791f57bfec703fac
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-07-07 22:08:53 +0300

    Better variable names

--- src/module_4/huffman_code.py
+++ src/module_4/huffman_code.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 import heapq
 from abc import abstractmethod, ABCMeta
 from collections import Counter
@@ -7,13 +5,13 @@ from typing import List, Tuple, Optional, Dict, Iterator
 
 
 class Node(metaclass=ABCMeta):
-    def __init__(self, value: int = 0, code: str = "") -> None:
-        self.value = value
+    def __init__(self, frequency: int = 0, code: str = "") -> None:
+        self.frequency = frequency
         self.code = code
 
     def __gt__(self, other: None) -> bool:
         assert isinstance(other, Node)
-        return self.value > other.value
+        return self.frequency > other.frequency
 
     @abstractmethod
     def walk(self, code: str = "") -> Iterator[Tuple[str, str]]:
@@ -46,28 +44,32 @@ class Tree:
         self.root = root
         self.string = string
         self._encode_dict = self._construct_encode_dict()
+        self.encoded_string = self._get_encoded_string()
 
     @classmethod
-    def from_string(cls, string: str) -> Tree:
+    def from_string(cls, string: str) -> "Tree":
         frequencies = Counter(string).most_common()
-        tree = Tree(root=Tree._construct_tree(frequencies), string=string)
-        return tree
+        return Tree(root=Tree._from_frequencies(frequencies), string=string)
 
-    def get_encoded_string(self) -> str:
+    def _get_encoded_string(self) -> str:
+        assert self._encode_dict
         return "".join(self._encode_dict[char] for char in self.string)
 
     @property
     def encode_dict_length(self) -> int:
+        assert self._encode_dict
         return len(self._encode_dict)
 
     def print_encode_dict(self) -> None:
+        assert self._encode_dict
         for k, v in self._encode_dict.items():
             print(f"{k}: {v}")
 
     @staticmethod
-    def _construct_tree(frequencies: List[Tuple[str, int]]) -> Node:
+    def _from_frequencies(frequencies: List[Tuple[str, int]]) -> Node:
         heap: List[Node] = [
-            Leaf(value=value, char=char) for (char, value) in frequencies
+            Leaf(frequency=frequency, char=char)
+            for (char, frequency) in frequencies
         ]
         heapq.heapify(heap)
 
@@ -77,9 +79,11 @@ class Tree:
         while len(heap) > 1:
             lf: Node = heapq.heappop(heap)
             ri: Node = heapq.heappop(heap)
-            lf.code += "0"
-            ri.code += "1"
-            branch = Branch(value=lf.value + ri.value, lf=lf, ri=ri)
+            lf.code = "0"
+            ri.code = "1"
+            branch = Branch(
+                frequency=lf.frequency + ri.frequency, lf=lf, ri=ri
+            )
             heapq.heappush(heap, branch)
 
         assert len(heap) == 1
@@ -92,11 +96,10 @@ class Tree:
 def main() -> None:
     string = input().strip()
     tree = Tree.from_string(string)
-    encoded_string = tree.get_encoded_string()
 
-    print(f"{tree.encode_dict_length} {len(encoded_string)}")
+    print(f"{tree.encode_dict_length} {len(tree.encoded_string)}")
     tree.print_encode_dict()
-    print(encoded_string)
+    print(tree.encoded_string)
 
 
 if __name__ == "__main__":
--- tests/module_4/test_huffman_encode.py
+++ tests/module_4/test_huffman_encode.py
@@ -3,9 +3,8 @@ import pytest
 from src.module_4.huffman_code import Tree
 
 
-@pytest.mark.parametrize("string,expected", [
-    ("a", "0"),
-    ("abacabad", "01001100100111"),
-])
+@pytest.mark.parametrize(
+    "string,expected", [("a", "0"), ("abacabad", "01001100100111")]
+)
 def test_encoding(string, expected):
-    assert Tree.from_string(string).get_encoded_string() == expected
+    assert Tree.from_string(string).encoded_string == expected
