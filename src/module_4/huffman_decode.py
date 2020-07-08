2e914a402245cb877713350888db7c5dce575150
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-07-08 22:17:12 +0300

    Process input

--- src/module_4/huffman_decode.py
+++ src/module_4/huffman_decode.py
@@ -1,55 +1,11 @@
-from abc import ABCMeta, abstractmethod
-from typing import Dict
-
-
-class Node(metaclass=ABCMeta):
-    @abstractmethod
-    def decode(self, encoded_string: str, pos: int) -> str:
-        ...
-
-
-class Branch(Node):
-    def __init__(self, lf: Node, ri: Node):
-        self.children = {"0": lf, "1": ri}
-
-    def decode(self, encoded_string: str, pos: int) -> str:
-        code = encoded_string[pos]
-        yield from self.children[code].decode(encoded_string, pos + 1)
-
-
-class Leaf(Node):
-    def __init__(self, char: str):
-        self.char = char
-
-    def decode(self, encoded_string: str, pos: int) -> str:
-        yield self.char
-
-
-class Tree:
-    def __init__(self, root: Node):
-        self.root = root
-
-    @classmethod
-    def from_dict(cls, decode_dict: Dict[str, str]) -> "Tree":
-        root = Branch()
-        for k, v in decode_dict.values():
-            pass
-        return Tree(root)
-
-    def decode(self, encoded_string: str) -> str:
-        return "".join(char for char in self.root.decode(encoded_string))
-
-
-def main():
-    nchars, encoded_length = map(int, input().split())
-    decode_dict = {}
-    for _ in range(nchars):
+def main() -> None:
+    unique_letters, length = [int(x) for x in input().strip().split()]
+    codes = {}
+    for _ in range(unique_letters):
         char, code = input().strip().split(": ")
-        decode_dict[code] = char
+        codes[char] = code
     encoded_string = input().strip()
-    tree = Tree.from_dict(decode_dict)
-    print(tree.decode(encoded_string))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
