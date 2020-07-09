69952586a3a9e6a104416e7d6e6fb7bb3bc4cf21
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-07-09 21:28:26 +0300

    Naive solution

--- src/module_4/huffman_decode.py
+++ src/module_4/huffman_decode.py
@@ -1,11 +1,28 @@
+from typing import Dict, List
+
+
+def decode(string: str, codes: Dict[str, str]) -> str:
+    decoded_string: List[str] = []
+    current_code: List[str] = []
+    for char in string:
+        current_code.append(char)
+        if char == "0":
+            decoded_string.append(codes["".join(current_code)])
+            current_code = []
+    if current_code:
+        decoded_string.append(codes["".join(current_code)])
+    return "".join(decoded_string)
+
+
 def main() -> None:
     unique_letters, length = [int(x) for x in input().strip().split()]
     codes = {}
     for _ in range(unique_letters):
         char, code = input().strip().split(": ")
-        codes[char] = code
+        codes[code] = char
     encoded_string = input().strip()
+    print(decode(encoded_string, codes))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
